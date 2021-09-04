44330d8fa58ac8d93b38c03b160d1092e4533ea4
dkersten <dkersten1994@gmail.com>
2021-09-04 12:06:12 -0400

    continued work on svg animations

---
+++ src/assets/js/animations.js
@@ -0,0 +1,11 @@
+new Vivus('experience-svg', {
+    type: 'oneByOne', 
+    duration: 50,
+    animTimingFunction: Vivus.LINEAR
+  });
+
+  new Vivus('previous-work-svg', {
+    type: 'oneByOne', 
+    duration: 50,
+    animTimingFunction: Vivus.LINEAR
+  });
