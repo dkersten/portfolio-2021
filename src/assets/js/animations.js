e37ef4ab0f68dcbb2d71595c656511daaec33657
dkersten <dkersten1994@gmail.com>
2021-09-04 15:42:49 -0400

    finished svg animations

--- src/assets/js/animations.js
+++ src/assets/js/animations.js
@@ -2,10 +2,24 @@ new Vivus('experience-svg', {
     type: 'oneByOne', 
     duration: 50,
     animTimingFunction: Vivus.LINEAR
-  });
+});
 
-  new Vivus('previous-work-svg', {
+new Vivus('previous-work-svg', {
     type: 'oneByOne', 
     duration: 50,
     animTimingFunction: Vivus.LINEAR
-  });
+});
+
+new Vivus('resources-outline', {
+    type: 'oneByOne', 
+    duration: 50,
+    animTimingFunction: Vivus.LINEAR
+});
+
+new Vivus('contact-outline', {
+    type: 'oneByOne', 
+    duration: 500,
+    animTimingFunction: Vivus.LINEAR
+});
+
+//   resources-outline
